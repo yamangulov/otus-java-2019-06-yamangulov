95a1bf456e56c0cafc3d0b6042ddece5409f2971
Iamangulov Andrey <yamangulov@gmail.com>
2019-10-05 09:45:31 +0300

    ДЗ-9 собственный jdbc orm исправление замечаний преподавателя удалил неиспользуемый класс и исключение из него

--- hw09-jdbc/src/main/java/ru/otus/jdbc/helpers/ReflectionsException.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.jdbc.helpers;
-
-public class ReflectionsException extends RuntimeException {
-    public ReflectionsException(){};
-
-    public ReflectionsException(String message) {
-        super(message);
-    }
-}
